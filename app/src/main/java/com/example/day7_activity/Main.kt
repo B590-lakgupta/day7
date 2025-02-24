74c18b9f357a88fec8c0b4e93dc18728fdb50e63
chsanise <chsanise@iu.edu>
2025-02-24 12:26:38 -0500

    Added Main.kt file with two functions milkCows() and feedChickens() with each counter incrementing after printing the values

---
+++ app/src/main/java/com/example/day7_activity/Main.kt
@@ -0,0 +1,30 @@
+package com.example.day7_activity
+
+fun main() {
+    milkCows()
+    feedChickens()
+}
+
+fun milkCows() {
+    var cow = 1
+    println("Milking cow #$cow")
+    cow += 1
+    println("Milking cow #$cow")
+    cow += 1
+    println("Milking cow #$cow")
+    cow += 1
+    println("Milking cow #$cow")
+    cow += 1
+}
+
+fun feedChickens() {
+    var chicken = 1
+    println("Feeding chicken #$chicken")
+    chicken += 1
+    println("Feeding chicken #$chicken")
+    chicken += 1
+    println("Feeding chicken #$chicken")
+    chicken += 1
+    println("Feeding chicken #$chicken")
+    chicken += 1
+}
