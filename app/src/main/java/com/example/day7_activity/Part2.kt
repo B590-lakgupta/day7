a09b9c7a0ef485a4fc832a7f22463bac2d6b0934
chsanise <chsanise@iu.edu>
2025-02-24 23:25:45 -0500

    Added day7-flows for slide 42

--- app/src/main/java/com/example/day7_activity/Part2.kt
+++ app/src/main/java/com/example/day7_activity/Part2.kt
@@ -3,18 +3,19 @@ package com.example.day7_activity
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.flow.transform
 import kotlinx.coroutines.runBlocking
 
 fun main() {
     runBlocking {
         milkMeasurement()
+            .take(2)  // Limit the number of emissions to 2
             .transform { x ->
                 emit("Receiving measurement: $x")
                 emit(format(x))
             }
             .collect { x -> println(x) }
-
         println("Done")
     }
 }
@@ -25,7 +26,7 @@ suspend fun format(measurement: Float): String {
 
 fun milkMeasurement(): Flow<Float> = flow {
     for (i in arrayOf(180F, 76F, 10F, 92F)) {
-        delay(timeMillis = 100)  // Delay of 100 milliseconds
+        delay(timeMillis = 100)
         emit(i)
     }
 }
