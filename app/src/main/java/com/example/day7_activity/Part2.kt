67dac7c4a711caf77b59357d057a795745e2f6ed
chsanise <chsanise@iu.edu>
2025-02-24 23:19:45 -0500

    Added day7-flows for slide 39

--- app/src/main/java/com/example/day7_activity/Part2.kt
+++ app/src/main/java/com/example/day7_activity/Part2.kt
@@ -4,23 +4,21 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeoutOrNull
 
 fun main() {
     runBlocking {
-        println("Calling milkMeasurement function...")
-        val flow = milkMeasurement()
-
-        println("Calling collect...")
-        flow.collect { x -> println(x) }
-
-        println("Calling collect again...")
-        flow.collect { x -> println(x) }
+        withTimeoutOrNull(timeMillis = 250) {
+            milkMeasurement().collect { x -> println(x) }
+        }
+        println("Done")
     }
 }
 
 fun milkMeasurement(): Flow<Float> = flow {
     for (i in arrayOf(180F, 76F, 10F, 92F)) {
-        delay(100)  // Delay of 100 milliseconds
+        delay(timeMillis = 100)  // Delay of 100 milliseconds
+        println("Emitting $i")
         emit(i)
     }
 }
