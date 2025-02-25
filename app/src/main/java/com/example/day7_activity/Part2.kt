d729e28e3c5141e65cbc56247f24f0663f0cfab2
chsanise <chsanise@iu.edu>
2025-02-24 23:09:16 -0500

    Added Main.kt for slide 36

--- app/src/main/java/com/example/day7_activity/Part2.kt
+++ app/src/main/java/com/example/day7_activity/Part2.kt
@@ -1,11 +1,21 @@
 package com.example.day7_activity
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-suspend fun main() {
-    milkMeasurement().collect { x -> println(x) }
+fun main() {
+    runBlocking {
+        launch {
+            for (k in 1..4) {
+                println("hello $k")
+            }
+        }
+
+        milkMeasurement().collect { x -> println(x) }
+    }
 }
 
 fun milkMeasurement(): Flow<Float> = flow {
